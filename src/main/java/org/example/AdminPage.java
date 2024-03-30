afb51674c3b82f014dd76e220ca27ada8be6bc3c
KISELEV Dmitry A <dmitry.a.kiselev@raiffeisen.ru>
2024-03-30 12:02:38 +0300

    Adding Tabs Checking

--- src/main/java/org/example/AdminPage.java
+++ src/main/java/org/example/AdminPage.java
@@ -7,10 +7,20 @@ import static org.example.LoginPage.getWebDriver;
 
 public class AdminPage {
 
-    private static String MENU_ITEM_PATH = "//div[@id='box-apps-menu-wrapper']//span[text()='%name%']";
+    private static String MENU_ITEM_PATH = "//div[@id='box-apps-menu-wrapper']//li[starts-with(@id,'app-')]";
+    private static String SUBMENU_ITEM_PATH = "//div[@id='box-apps-menu-wrapper']//li[starts-with(@id,'doc-')]";
     private static String HEADER = "//h1";
-    public static void clickMenuItem(String menuItemName) {
-        getWebDriver().findElement(By.xpath(MENU_ITEM_PATH.replace("%name%", menuItemName))).click();
+    public static void checkHeaders() {
+        int menuItemsCounter = getWebDriver().findElements(By.xpath(MENU_ITEM_PATH)).size();
+        for (int i = 1; i <= menuItemsCounter; i++) {
+            getWebDriver().findElement(By.xpath(MENU_ITEM_PATH + "[" + i + "]")).click();
+            int menuSubItemsCounter = getWebDriver().findElements(By.xpath(SUBMENU_ITEM_PATH)).size();
+            checkHeader();
+            for (int j = 2; j <= menuSubItemsCounter; j++) {
+                getWebDriver().findElement(By.xpath(SUBMENU_ITEM_PATH + "[" + j + "]")).click();
+                checkHeader();
+            }
+        }
     }
 
     public static void checkHeader() {
--- src/test/java/org/example/TabsCheckTest.java
+++ src/test/java/org/example/TabsCheckTest.java
@@ -5,18 +5,9 @@ import java.util.List;
 
 public class TabsCheckTest {
 
-    private List<String> TABS = List.of( "Appearence","Logotype","Catalog","Product Groups","Option Groups","Manufacturers","Suppliers","Delivery Statuses",
-            "Sold Out Statuses","Quantity Units","CSV Import/Export","Countries","Currencies","Customers","CSV Import/Export","Newsletter","Geo Zones","Languages","Storage Encoding",
-            "Modules","Customer","Shipping","Payment","Order Total","Order Success","Order Action","Orders","Order Statuses","Pages","Reports","Most Sold Products","Most Shopping Customers",
-            "Settings","Defaults","General","Listings","Images","Checkout","Advanced","Security","Slides","Tax","Tax Rates","Translations","Scan Files","CSV Import/Export","Users","vQmods");
     @Test
     public void checkTabsTest(){
         LoginPage.loginAsAdmin();
-        TABS.forEach( tab -> {
-            AdminPage.clickMenuItem(tab);
-            AdminPage.checkHeader();
-        });
-
-
+        AdminPage.checkHeaders();
     }
 }
